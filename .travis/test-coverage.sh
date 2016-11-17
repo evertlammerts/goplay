97b64f44a1abb15a654d26b190399f93b09a4d2a
Evert Lammerts <evert@lucipher.net>
2016-11-17 11:11:42 +0100

    added test coverage with coveralls.io and integrated with travis

---
+++ .travis/test-coverage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+testCover() {
+	# set the return value to 1 (succesful)
+	retval=0
+	# get the directory to check from the parameter. Default to '.'
+	d=${1:-.}
+	# skip if there are no Go files here
+	ls $d/*.go &> /dev/null || return $retval
+	# switch to the directory to check
+        pushd $d > /dev/null
+	# create the coverage profile
+	coverageresult=`go test -v -coverprofile=profile.out`
+	# output the result so we can check the shell output
+	echo ${coverageresult}
+	# append the results to acc.out if coverage didn't fail, else set the retval to 1 (failed)
+	( [[ ${coverageresult} == *FAIL* ]] && retval=1 ) || ( [ -f profile.out ] && grep -v "mode: set" profile.out >> acc.out )
+	# return to our working dir
+	popd > /dev/null
+	# return our return value
+	return $retval
+}
+
+# Init acc.out
+echo "mode: set" > acc.out
+
+# Run test coverage on all directories containing go files
+find . -maxdepth 10 -type d | while read d; do testCover $d || exit; done
+
+# Upload the coverage profile to coveralls.io
+[ -n "$COVERALLS_TOKEN" ] && goveralls -coverprofile=acc.out -service=travis-ci -repotoken $COVERALLS_TOKEN
+
